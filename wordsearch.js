134b358893be057f280be67b629eea7c48055206
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-07-03 15:51:04 -0600

    peer clint

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = (word) => {
+  let result = [];
 
-module.exports = wordSearch
+  for (let i = 0; i < word.length; i++) {
+    let rows = [];
+
+    for (let j = 0; j < word.length; j++) {
+      rows.push(word[j][i]);
+    }
+    result.push(rows);
+  }
+  return result;
+};
+
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalLetters = transpose(letters);
+  const verticalJoin = verticalLetters.map((ls) => ls.join(""));
+
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+
+};
+
+
+module.exports = wordSearch;
